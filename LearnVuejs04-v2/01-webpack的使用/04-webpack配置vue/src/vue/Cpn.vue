9bc58f5af167107a2d8e75a797ae39bc991f0807
tanwb <tanwb@yansu.com>
2021-01-29 11:13:12 +0800

    update

---
+++ LearnVuejs04-v2/01-webpack的使用/04-webpack配置vue/src/vue/Cpn.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>我是Cpn组件的标题</h2>
+        <p>我是Cpn组件的内容：hhhhhhhhhhhh</p>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Cpn",
+        data(){
+            return {
+                name: 'cpn组件的name'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
